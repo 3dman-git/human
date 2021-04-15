8f6dfeafe20c2650ee2bdd2bab687d80d9681e96
3dmain-git <466322940@qq.com>
2021-04-15 16:16:52 +0800

    update

--- qCC/pluginManager/ccPluginInfoDlg.h
+++ qCC/pluginManager/ccPluginInfoDlg.h
@@ -1,21 +1,5 @@
 #ifndef CCPLUGININFODLG_H
 #define CCPLUGININFODLG_H
-//##########################################################################
-//#                                                                        #
-//#                              CLOUDCOMPARE                              #
-//#                                                                        #
-//#  This program is free software; you can redistribute it and/or modify  #
-//#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
-//#                                                                        #
-//#  This program is distributed in the hope that it will be useful,       #
-//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
-//#  GNU General Public License for more details.                          #
-//#                                                                        #
-//#          COPYRIGHT: CloudCompare project                               #
-//#                                                                        #
-//##########################################################################
 
 #include <QDialog>
 #include <QList>
--- qCC/pluginManager/ccPluginUIManager.h
+++ qCC/pluginManager/ccPluginUIManager.h
@@ -1,23 +1,6 @@
 #ifndef CCPLUGINUIMANAGER_H
 #define CCPLUGINUIMANAGER_H
 
-//##########################################################################
-//#                                                                        #
-//#                              CLOUDCOMPARE                              #
-//#                                                                        #
-//#  This program is free software; you can redistribute it and/or modify  #
-//#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
-//#                                                                        #
-//#  This program is distributed in the hope that it will be useful,       #
-//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
-//#  GNU General Public License for more details.                          #
-//#                                                                        #
-//#          COPYRIGHT: CloudCompare project                               #
-//#                                                                        #
-//##########################################################################
-
 #include <QActionGroup>
 #include <QList>
 #include <QObject>
