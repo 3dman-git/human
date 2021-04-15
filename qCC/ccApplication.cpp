d818e198558acdb8656fdbdfdfcaeb5f8a1af871
3dmain-git <466322940@qq.com>
2021-04-15 15:51:21 +0800

    update

--- qCC/ccApplication.cpp
+++ qCC/ccApplication.cpp
@@ -1,20 +1,3 @@
-//##########################################################################
-//#                                                                        #
-//#                              CLOUDCOMPARE                              #
-//#                                                                        #
-//#  This program is free software; you can redistribute it and/or modify  #
-//#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
-//#                                                                        #
-//#  This program is distributed in the hope that it will be useful,       #
-//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
-//#  GNU General Public License for more details.                          #
-//#                                                                        #
-//#          COPYRIGHT: CloudCompare project                               #
-//#                                                                        #
-//##########################################################################
-
 #include <QtGlobal>
 
 #ifdef Q_OS_MAC
@@ -30,7 +13,7 @@
 ccApplication::ccApplication( int &argc, char **argv, bool isCommandLine )
 	: ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "2.11.3 (Anoia)" ) )
 {
-	setApplicationName( "CloudCompare" );
+    setApplicationName( "CloudCompare" );
 	
 	FileIO::setWriterInfo( applicationName(), versionStr() );
 }
--- qCC/ccColorScaleEditorWidget.cpp
+++ qCC/ccColorScaleEditorWidget.cpp
@@ -1,22 +1,3 @@
-//##########################################################################
-//#                                                                        #
-//#                              CLOUDCOMPARE                              #
-//#                                                                        #
-//#  This program is free software; you can redistribute it and/or modify  #
-//#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 or later of the License.      #
-//#                                                                        #
-//#  This program is distributed in the hope that it will be useful,       #
-//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
-//#  GNU General Public License for more details.                          #
-//#                                                                        #
-//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
-//#                                                                        #
-//##########################################################################
-
-//Inspired from ccColorScaleEditorWidget by Richard Steffen (LGPL 2.1)
-
 #include "ccColorScaleEditorWidget.h"
 
 //Qt
