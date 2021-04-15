940c8d26ad275e5aeb685a5af69d07108da94c19
3dmain-git <466322940@qq.com>
2021-04-15 14:25:20 +0800

    update

--- ccViewer/ccViewerApplication.cpp
+++ ccViewer/ccViewerApplication.cpp
@@ -11,7 +11,7 @@
 ccViewerApplication::ccViewerApplication( int &argc, char **argv, bool isCommandLine )
     : ccApplicationBase( argc, argv, isCommandLine, QStringLiteral( "1.0" ) )
 {
-	setApplicationName( "CloudCompareViewer" );
+    setApplicationName( "senlion3DHuman" );
 }
 
 void ccViewerApplication::setViewer(ccViewer *inViewer)
